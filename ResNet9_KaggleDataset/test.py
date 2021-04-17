eb24d55607c4db40649a5d76286d3dd01bf5d0b3
hhhfccz <hjk1938927583@163.com>
2021-04-17 23:25:25 +0800

    Update test.py
    add onnxruntime

--- ResNet9_KaggleDataset/test.py
+++ ResNet9_KaggleDataset/test.py
@@ -1,16 +1,32 @@
 from config import *
 from model import *
+
+import numpy as np
 import torch
+import torchvision
+import onnxruntime
+import time
+
 
 def predict_image(img, model):
-    xb = to_device(img.unsqueeze(0), device)
-    yb = model(xb)
-    _, preds  = torch.max(yb, dim=1)
-    return train.classes[preds[0].item()]
+    xb = to_device(img.unsqueeze_(0), device="cpu")
+    # yb = model(xb)
+    yb = model.run([], {"input": xb.numpy()})
+    # print(yb)
+    yb = np.array(yb)
+    pred = np.max(yb)
 
-test_dir = "/content/plant_diseases_detection/ResNet9_KaggleDataset/dataset/test/"
-test = ImageFolder(test_dir, transform=transforms.ToTensor())
+    return int(np.where(yb==pred)[-1])
+
+test_dir = "./dataset/test"
+test = ImageFolder(test_dir, transform=torchvision.transforms.ToTensor())
 test_images = sorted(os.listdir(test_dir + '/test'))
+model = onnxruntime.InferenceSession("./ResNet9_KaggleDataset.onnx")
+train_dir, valid_dir = config()
+train, _, _, _ = get_data(train_dir, valid_dir)
 
 for i, (img, label) in enumerate(test):
-    print('Label:', test_images[i], ', Predicted:', predict_image(img, model))
+    t0 = time.time()
+    pred = predict_image(img, model)
+    t1 = time.time()
+    print('Label:', test_images[i], '\t', 'Predicted:', train.classes[pred], '\t', 'Time:', str(t1-t0))
